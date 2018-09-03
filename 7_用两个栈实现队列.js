62c157b3f49087eaabcda9b44a7ae88894aa0b7a
Voyz <260022624@qq.com>
2018-09-03 23:39:30 +0800

    JS

---
+++ 7_用两个栈实现队列.js
@@ -0,0 +1,33 @@
+// 需要两个栈Stack1和Stack2，
+// push的时候直接push进Stack1。
+// pop需要判断Stack1和Stack2中元素的情况，
+// Stack1空的话，直接从Stack2 pop
+// Stack1不空的话，把Stack1的元素push进入Stack2，
+// 然后pop Stack2的值
+
+var Stack1 = [];
+var Stack2 = [];
+
+var push = function(p){
+	Stack1.push(p);
+}
+var pop = function() {
+	if (Stack1.length === 0 && Stack2.length == 0) {
+		return;
+	}else if (Stack1.length === 0) {
+		Stack2.pop();
+	}else{
+		while(Stack1.length > 0) {
+			Stack2.push(Stack2.pop());
+		}
+	}
+	Stack2.pop();
+	}
+}
+
+push(1);
+push(2);
+push(4);
+push(6);
+push(5);
+pop();
